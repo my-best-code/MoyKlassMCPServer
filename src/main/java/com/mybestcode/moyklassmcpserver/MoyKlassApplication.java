802559ece72c1b6c58630c2f7b9936645050d8ae
Dmitri Konnov <dmitri.v.konnov@gmail.com>
2025-07-20 12:14:39 +0400

    setting up the project

---
+++ src/main/java/com/mybestcode/moyklassmcpserver/MoyKlassApplication.java
@@ -0,0 +1,14 @@
+package com.mybestcode.moyklassmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MoyKlassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoyKlassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mybestcode/moyklassmcpserver/MoyKlassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybestcode.moyklassmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyKlassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
